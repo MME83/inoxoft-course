79c8efd95824007290265abb6f7ec848351dda9e
Mykola Moroz <moroz.mykola@gmail.com>
2021-08-19 01:05:07 +0300

    feat: add v2 of task, add app2 file, create folders with random files and add info to readme

---
+++ inoxoft-task1/app2.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const path = require('path');
+
+const boysPath = path.join(__dirname, 'boys');
+const girlsPath = path.join(__dirname, 'girls');
+
+fs.readdir(boysPath, (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    
+    if (files.length === 0) {
+        console.log('No files in directory');
+        return;
+    } 
+
+    console.log("List of all files in folder boys: \n", files, "\n");
+
+    files.forEach(file => {
+        const currentFilePath = path.join(boysPath, file);
+
+        fs.readFile(currentFilePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            if (!JSON.parse(data).gender) 
+            return console.log(`Gender in ${file} has not found`);
+
+            if (JSON.parse(data).gender === 'female') {
+            
+                fs.rename(currentFilePath, girlsPath + '/' + file, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+        
+                    console.log(`${file} has moved to folder girls`);
+                });
+            } else if (JSON.parse(data).gender === 'male') {
+                console.log(`${file} still in folder boys`);
+                return;                  
+            }
+        });
+    });
+});
+
+fs.readdir(girlsPath, (err, files) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    
+    if (files.length === 0) {
+        console.log('No files in directory');
+        return;
+    } 
+
+    console.log("List of all files in folder boys: \n", files, "\n");
+
+    files.forEach(file => {
+        const currentFilePath = path.join(girlsPath, file);
+
+        fs.readFile(currentFilePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            if (!JSON.parse(data).gender) 
+            return console.log(`Gender in ${file} has not found`);
+
+            if (JSON.parse(data).gender === 'male') {
+            
+                fs.rename(currentFilePath, boysPath + '/' + file, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+        
+                    console.log(`${file} has moved to folder boys`);
+                });
+            } else if (JSON.parse(data).gender === 'female') {
+                console.log(`${file} still in folder girls`);
+                return;                  
+            }
+        });
+    });
+});
--- inoxoft-task1/readme.md
+++ inoxoft-task1/readme.md
@@ -1,4 +1,4 @@
-#InoXoft Task-1
+#InoXoft Task-1 v1
 
 Upload or create some files *.txt/*.json in folder: `upload` with random data inside, like example: {"gender":"female"} or {"gender":"male"}
 
@@ -6,7 +6,6 @@ Upload or create some files *.txt/*.json in folder: `upload` with random data in
 ``` 
 moving files by gender to custom folder "../file-bd/girls/" or "../file-bd/boys/"
 ```
-
 
 ##Download folder inoxoft-task1 from PR to your PC!!!
 ```
@@ -19,3 +18,20 @@ node app.js
 ###Run app/program auto -> (download folder `inoxoft-task1` to your PC)
 ```
 npm start
+```
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+#InoXoft Task-1 v2
+
+You have in main root two folders: boys & girls with random files *.txt/*.json, every file can has some gender type inside like like example: {"gender":"female"} or {"gender":"male"}
+
+##Result
+```
+sort all files to boys or girls folder by gender
+```
+
+###Run app/program
+```
+npm start-v2 or node app2.js
+```
