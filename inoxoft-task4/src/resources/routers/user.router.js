2c2c877eab0a848433536f49cb6d08df3c1a4829
Mykola Moroz <moroz.mykola@gmail.com>
2021-09-01 22:59:24 +0300

    feat: add one middleware to users router for checking if email is unique when user updating

--- inoxoft-task4/src/resources/routers/user.router.js
+++ inoxoft-task4/src/resources/routers/user.router.js
@@ -3,15 +3,23 @@ const router = require('express').Router();
 const userController = require('../controllers/user.controller');
 const userMiddleware = require('../../middleware/user.middleware');
 
-router.get('/:user_id', userMiddleware.isUserIdValid, userController.getUserById);
 router.get('/', userController.getAllUsers);
-router.post('/', userMiddleware.isReqBodyInSignupValid, userMiddleware.isEmailExists, userController.createUser);
+
+router.post(
+    '/',
+    userMiddleware.isReqBodyInSignupValid,
+    userMiddleware.isEmailExists,
+    userController.createUser
+);
+
+router.get('/:user_id', userMiddleware.isUserIdValid, userController.getUserById);
 
 router.patch(
     '/:user_id',
     userMiddleware.isUserIdValid,
     userMiddleware.isReqBodyUpdateValid,
     userMiddleware.isUserByIdExists,
+    userMiddleware.isEmailExists,
     userController.updateUser
 );
 
