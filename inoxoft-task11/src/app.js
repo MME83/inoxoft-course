4fef1008b8d17333e9fa31851392de5a3dfa1dac
Mykola Moroz <moroz.mykola@gmail.com>
2021-09-12 17:51:46 +0300

    feat: add reate limit to requests frim one IP

--- inoxoft-task11/src/app.js
+++ inoxoft-task11/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressFileUpload = require('express-fileupload');
+const rateLimit = require('express-rate-limit');
 
 const {
     authRouter,
@@ -12,6 +13,10 @@ const handleErrors = require('./middleware/globalHandleErrors');
 
 const app = express();
 
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 100 requests per windowMs
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(expressFileUpload());
